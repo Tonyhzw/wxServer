ce32953ace52acf82f784947663c3aa2da4e3457
tonyhzw <tony_join@163.com>
2017-06-14 11:14:12 +0800

    fixed some errors

--- server.js
+++ server.js
@@ -261,23 +261,23 @@ app.get('/getExpress',function(req,res){
 app.get('/getOrderDetail',function(req,res){
   //先按orderId查到所有关联的书籍，然后将书籍按照运单号分类
   var orderId = req.query.orderId,sql = "",orderStateList = ['待借出','借出','寄回','已完成'];
-  sql = "select orderState,mailNumber from bookOrder where orderId = "+mysql.escape(orderId)+";";
+  sql = "select bookOrder.orderState,bookOrder.mailNumber,orderTable.userId from bookOrder,orderTable where bookOrder.orderId = "+mysql.escape(orderId)+" and orderTable.orderId = "+mysql.escape(orderId)+";";
   query(sql,function(err,vals,fields){
     var datalist = vals,results = [];
     datalist.forEach(function(val,index){
-      var mailNumber = val.mailNumber, orderState = val.orderState;
+      var mailNumber = val.mailNumber, orderState = val.orderState,userId = val.userId;
       if(orderState == 0){
-        sql = "select book.* from bookOrder,book where bookOrder.mailNumber is "+mailNumber+
+        sql = "select bookOrder.bookOrderId,book.* from bookOrder,book where bookOrder.mailNumber is "+mailNumber+
         " and bookOrder.bookId = book.bookId;";
         query(sql,function(err,vals,fields){
-          results.push({mailNumber:mailNumber,status:orderStateList[orderState],book:vals});
+          results.push({userId:userId,mailNumber:mailNumber,status:orderStateList[orderState],book:vals});
           if(index == datalist.length-1) res.json({success:true,bookList:results});
         })
       }else{
-        sql = "select book.* from bookOrder,book where bookOrder.mailNumber = "+mailNumber+
+        sql = "select bookOrder.bookOrderId,book.* from bookOrder,book where bookOrder.mailNumber = "+mailNumber+
         " and bookOrder.bookId = book.bookId;";
         query(sql,function(err,vals,fields){
-          results.push({mailNumber:mailNumber,status:orderStateList[orderState],book:vals});
+          results.push({userId:userId,mailNumber:mailNumber,status:orderStateList[orderState],book:vals});
           if(index == datalist.length-1) res.json({success:true,bookList:results});
         })
       }
